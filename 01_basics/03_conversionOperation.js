1f0b2551ea1c74eabac154bb0153d4c0b67f113a
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-06 19:06:35 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "33";
+let value = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof value);
+// console.log(typeof(score));
+
+let valueInNo = Number(score);
+
+// console.log(typeof valueInNo);
+// console.log(Number(value));
+// console.log(value);
+
+let xyz = null;
+// console.log(typeof xyz);
+// console.log(Number(xyz));
+
+// "number" -> Number
+// "33abc" -> NaN
+// true -> 1
+// false -> 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let word = "";
+// console.log(Boolean(word));
+
+let notEmpty = "Priyal";
+// console.log(Boolean(notEmpty));
+
+// 1 -> true; 0 -> false
+// "" -> false; "string" -> true
+
+let someNum = 33;
+let someNumString = String(someNum);
+console.log(someNumString);
+console.log(typeof someNumString);
+
+
+
+
+
+
+
+
+
