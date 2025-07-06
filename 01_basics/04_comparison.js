409ffaeabc44eab560bfffe788eeaa07b7f0308b
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-06 19:58:29 +0000

    comparison operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log("2" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+
+console.log("2" === 2);    
+// === strict checks the value and its data type also that is why the above line will result in false o/p
+
+
+
+
+
