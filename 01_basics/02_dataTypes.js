4337660971625bc20a519285b30df0b6e1594e36
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-06 18:30:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; 
+// Treat all JS code as newer version
+
+// alert(3 + 3), we are using node js not browser
+// alert("Hello")
+
+console.log("Priyal");
+
+let name = "Priyal";
+let age = 20;
+let isLoggedIn = true;
+let state = null;
+
+
+// number -> 2^53
+// string -> ""
+// bigint
+// boolean -> true / false
+// null -> standalone value (empty)
+// undefined -> declared a variable  but not assigned any value.
+// symbol -> uniquness
+
+// object ->
+
+console.log(typeof "Priyal");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
