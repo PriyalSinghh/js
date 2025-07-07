0d9dda7e3ccd301ac1001d9dbd000de611746605
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-07 18:15:10 +0000

    Numbers and Maths done

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,34 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(typeof(balance.toString()));
+// console.log(balance.toFixed(2));
+
+const num = 23.4555;
+// console.log(num.toPrecision(3));
+
+const hund = 1000000;
+// console.log(hund.toLocaleString());
+// console.log(hund.toLocaleString('en-IN'));
+
+// ********** Maths **************//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.39));
+// console.log(Math.ceil(3.39));
+// console.log(Math.min(0,4,2,3));
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()* (max - min + 1)) + min);
+
+
+
+
+
+
+
