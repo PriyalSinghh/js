d4e961c6aeb487cb28369d41b36efd810a1ed904
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-07 20:25:26 +0000

    Arrays basics done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+const myArr = [0, 1, 2, 3, 4, 5, 6];
+// console.log(myArr[0]);
+
+const heroes  = ["Ironman", "Thor"];
+const arr2 = new Array(1,2,3,4);
+
+// Array methods---
+
+myArr.push(10);
+// console.log(myArr);
+myArr.pop();
+// console.log(myArr);
+myArr.unshift(9);
+// console.log(myArr);
+myArr.shift();
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+ const newArr = myArr.join();
+//  console.log(myArr);
+//  console.log(typeof newArr);
+
+console.log("A",myArr);
+const arr3 = myArr.slice(1,3);
+console.log(arr3);
+console.log("B",myArr);
+
+const arr4 = myArr.splice(1,3);
+console.log(arr4);
+console.log("C",myArr);
+
+
+
+
+
+ 
+ 
+
+
+
+
+
