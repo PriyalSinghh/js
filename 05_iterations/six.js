3f7d4386e725a7de8c6f6e7b638dc32ab940f1ac
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-11 19:10:44 +0000

    for-each loop done

---
+++ 05_iterations/six.js
@@ -0,0 +1,22 @@
+// const coding = ["js", "java","cpp", "python"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     return item
+// })
+// for-each loop do not return anything, it gives undefined
+// console.log(values);
+
+// const nums = [1, 2, 3, 4, 5, 6, 7]
+// const newNums = nums.filter( (item) => item > 5) //here we are not using {} brackets so no need to return 
+// console.log(newNums);
+
+// filter returns the values unlike for-each loop
+
+// const newNums = nums.filter( (item) => {
+//     return item // here we are using {}, so return keyword is must.
+// })
+// console.log(newNums);
+
+
+
