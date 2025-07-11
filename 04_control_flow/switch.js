037c8fdc2873f4e3a177722bc2db59630684a2cf
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-11 17:03:07 +0000

    switch  control flow done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month = 10;
+
+switch(month) {
+    case 1:
+        console.log("January");
+        break; // break because whenever any case matches then without break statement it executes all other cases excpet default also.
+    case 2:
+        console.log("Fabruary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("Not a month");
+        //break;
+}
