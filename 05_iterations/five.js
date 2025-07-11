7f24cc9b80192fb17c47c4f1f82dbe1ddbc65bf4
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-11 18:56:46 +0000

    for each loop on objects

---
+++ 05_iterations/five.js
@@ -0,0 +1,44 @@
+// for-each loop
+
+const coding = ["rb", "java", "python", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+//     // the function which is passed here it is called as callback function,
+//     // it is same as the normal js function but it do not have any name.
+// })
+
+// coding.forEach( (value) => {
+//     console.log(value);
+// })
+
+// function printMe(item) {
+//     console.log(item);
+// }
+// coding.forEach(printMe) // only provide reference of the fxn i.e. printMe not the paranthesis else it will get executed.
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+    
+// })
+
+// const myCoding = [
+//     {
+//         languageName: "JS",
+//         languageFile: "js"
+//     },
+//     {
+//         languageName: "Java",
+//         languageFile: "java"
+//     },
+//     {
+//         languageName: "Python",
+//         languageFile: "py"
+//     },
+
+// ]
+
+// myCoding.forEach( (item) => {
+//     console.log(item.languageName);
+//     console.log(item.languageFile);
+// })
