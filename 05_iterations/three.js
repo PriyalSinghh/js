67d38478449ef15bd9998d7298a8ebb925f47859
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-11 18:34:47 +0000

    for-of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,36 @@
+// for of loop for arrays
+
+// const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello World!"
+// for (const it of greetings) {
+//    console.log(it);
+// }
+
+// maps,,, do not contains duplicate values in this
+
+// const map = new Map()
+// map.set("IN", "INDIA")
+// map.set("US", "UNITED STATES")
+// map.set("FR", "FRANCE")
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key, '-', value);
+     
+// }
+
+// const myObj = {
+//     game1: "NFS",
+//     game2: "Spiderman"
+// }
+
+// for (const element of myObj) {
+//     console.log(element); // not iteratable this way.
+       
+// }
+
