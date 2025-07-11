29ca55ee313fc04c0ebeba9550c41566e5954695
PriyalSinghh <129093844+PriyalSinghh@users.noreply.github.com>
2025-07-11 18:41:53 +0000

    for-in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+// const myObj = {
+//     js: "Javascript",
+//     cpp: "C Plus Plus",
+//     rb: "Ruby",
+//     swift: "Swift by Apple"
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key} for ${myObj[key]}`);
+// }
+
+// const arr = ["js", "ruby", "python", "java", "cpp"]
+// for (const key in arr) {
+//     // console.log(key); // will print the indices unlike for-of loop
+//     console.log(arr[key]);
+// }
+
+// map is not iteratable by for-in loop
